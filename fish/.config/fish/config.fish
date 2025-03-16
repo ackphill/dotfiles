f60922a73f0455e63fbb3ca8561c67313a5578e0
Jung Sangwoo <holyavarice@gmail.com>
2025-03-16 14:00:28 +0900

    fix: merge fish config among pc's

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,4 @@
-set fish_greeting ""
+set -U fish_greeting
 if status is-interactive
     # Commands to run in interactive sessions can go here
     # fish_vi_key_bindings
@@ -6,9 +6,18 @@ end
 
 # >>> mamba initialize >>>
 # !! Contents within this block are managed by 'mamba init' !!
-set -gx MAMBA_EXE "/home/j/.local/bin/micromamba"
-set -gx MAMBA_ROOT_PREFIX "/home/j/micromamba"
-$MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
+set -gx MAMBA_EXE "~/.local/bin/micromamba"
+set -gx MAMBA_ROOT_PREFIX "~/micromamba"
+if test -e $MAMBA_EXE
+    $MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
+end
 # <<< mamba initialize <<<
 
-thefuck --alias | source
+if command -v thefuck >/dev/null 2>&1
+    thefuck --alias | source
+end
+
+if test -f ~/.common_aliases
+    source ~/.common_aliases
+end
+
